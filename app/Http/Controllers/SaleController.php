15fb1e2bc866976f58d37fc5f8928167c94806e0
Vilaca <khallil.vilaca@gmail.com>
2024-03-04 17:44:15 -0300

    first commit

---
+++ app/Http/Controllers/SaleController.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class SaleController extends Controller
+{
+    public function createNewSaleAction(Response $response, Request $request)
+    {
+
+    }
+}
